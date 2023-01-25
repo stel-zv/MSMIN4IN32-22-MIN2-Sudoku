b6d4c0fe5793148adf0c10a0819b0bb2235c8697
GuillaumeBdrd <guillaume.bodard@orange.fr>
2023-01-25 16:23:11 +0100

    Ajout solver Graph vide

---
+++ Sodoku.GraphColoring/SolverGraphColoring.cs
@@ -0,0 +1,18 @@
+﻿using Sudoku.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Sudoku.GraphColoring 
+{ 
+    public class SolverGraphColoring : ISudokuSolver
+    {
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            return s.CloneSudoku();
+        }
+    }
+}
