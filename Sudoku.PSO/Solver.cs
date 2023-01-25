8384b9bc95091eb32eeffb8a60569c8847cc12f5
AurelienBltx <aurelien.bltx@gmail.com>
2023-01-25 16:21:39 +0100

    Création classe PSO empty Solver

---
+++ Sudoku.PSO/Solver.cs
@@ -0,0 +1,17 @@
+﻿using Sudoku.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku.PSO
+{
+    public class PSOSolver : ISudokuSolver
+    {
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            return s;
+        }
+    }
+}
