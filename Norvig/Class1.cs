83b07b9b849bfb57569444dc03f2d378b3dfb18f
theog <theog@MSI>
2023-01-25 16:01:16 +0100

    Ajout class SudokuNorvig

---
+++ Norvig/Class1.cs
@@ -0,0 +1,12 @@
+﻿using Sudoku.Shared;
+
+namespace Norvig
+{
+    public class SudokuNorvig : ISudokuSolver
+    {
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            return s.CloneSudoku();
+        }
+    }
+}
