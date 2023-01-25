46369febcaa2f6418b1c3d197481e54f55748ca6
leaGhk <lea.gks@gmail.com>
2023-01-25 16:06:40 +0100

    Ajout SolverCSP vide

---
+++ SolverCSPLea/CSPSolver.cs
@@ -0,0 +1,12 @@
+﻿using Sudoku.Shared;
+
+namespace SolverCSPLea
+{
+    public class CSPSolver : ISudokuSolver
+    {
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            return s.CloneSudoku();
+        }
+    }
+}
