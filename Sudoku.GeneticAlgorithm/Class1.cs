20bb0f7beb53c22e891aef480a5ea3406dd6e840
ttnchpln <titouan.chapalain@gmail.com>
2023-02-08 16:57:03 +0100

    Push 1

---
+++ Sudoku.GeneticAlgorithm/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.GeneticAlgorithm
+{
+    public class GeneticAlgorithmSolver : ISudokuSolver
+    {
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            return s;
+            // test
+        }
+    }
+}
